452ba91c79acde45197cbbf73ef4b31085f8fa24
juansvelasco22 <js.velasco@uniandes.edu.co>
2020-05-27 17:46:26 -0400

    Subir archivo a Git

---
+++ HW09/funciones.h
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int suma(int a, int b)
+{
+    cout<<"Esta es la funcion suma"<<endl;
+    return a+b;
+}
---
+++ HW09/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "funciones.h"
+
+using namespace std;
+
+//Comentarios cortos
+/*
+Comentarios largos
+*/
+
+
+int main()
+{
+    
+    cout<<"Hola mundo"<<endl;
+    
+    int x=4, y=5;
+    
+    cout<<suma(x,y)<<endl;
+    return 0;
+    
+}
+    
